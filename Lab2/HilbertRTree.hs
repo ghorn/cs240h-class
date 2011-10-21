a8d884f54e776694fd0b96d4dd8544bd9edb5d24
Greg Horn <gregmainland@gmail.com>
2011-10-20 19:08:26 -0700

    this is broken but I'm about to refactor

--- Lab2/HilbertRTree.hs
+++ Lab2/HilbertRTree.hs
@@ -12,9 +12,10 @@ import Lab2.HilbertCurve(xy2d)
 import Data.List(foldl', insertBy)
 import Debug.Trace
 
---cL,cN :: Int
---cL = 3
---cN = 4
+cL,cN :: Int
+cL = 3
+cN = 4
+
 hilbertDim :: Int
 hilbertDim = 16 -- for max value of 65535
 
@@ -24,15 +25,18 @@ data HRect = HRect Int Rect deriving Show
 fromRect :: Rect -> HRect
 fromRect rect = HRect (hilbertValue rect) rect
 
-
-data Node = Empty |
-            NonLeaf { lhv   :: Int
-                    , mbr   :: Rect
-                    , nodes :: [Node]
+data Node = Root |
+            NonLeaf { supertree :: [Node]
+                    , lhv       :: Int
+                    , mbr       :: Rect
+                    , children  :: [Node]
                     } |
-            Leaf { lhv :: Int
+            Leaf { parent :: Node
+                 , lhv    :: Int
+                 , mbr    :: Rect
                  , hrects ::[HRect]
                  } deriving Show
+
 
 
 hilbertValue :: Rect -> Int
@@ -41,27 +45,57 @@ hilbertValue rect = xy2d hilbertDim (hx, hy)
     hx = rectMinX rect + ((rectMaxX rect - rectMinX rect) `div` 2)
     hy = rectMinY rect + ((rectMaxY rect - rectMinY rect) `div` 2)
 
+emptyHRTree :: Node
+emptyHRTree = Root
 
 newHRTree :: [Rect] -> Node
 newHRTree = foldl' (\acc x -> insert x acc) emptyHRTree
 
-emptyHRTree :: Node
-emptyHRTree = Empty
+--makeBalancedNode :: [HRect] -> Node
+--makeBalancedNode hrects = aNode
+--  where
+--    n = length hrects
+--    while
+--    logbase cN
+    
+--largestHilbertValue :: Node -> Int
+--largestHilbertValue leaf@(Leaf {})
+
+isFull :: Node -> Bool
+isFull leaf@(Leaf {})
+  | length (hrects leaf) > cL  = error "leaf somehow got too many entries, this is a bug"
+  | length (hrects leaf) == cL = True
+  | otherwise                  = False
+isFull nonleaf@(NonLeaf {}) = all isFull $ children nonleaf
+isFull (Root {}) = error "calling isFull on Root"
+
 
 insert :: Rect -> Node -> Node
-insert rect Empty = Leaf {lhv = hilbertValue rect, hrects = [fromRect rect]}
-insert rect nonleaf@(NonLeaf _ _ _) = insert rect (chooseLeaf rect nonleaf)
-insert rect leaf = Leaf { lhv = maximum $ map (\(HRect h _) -> h) newHRects
-                        , hrects = newHRects
-                        }
+insert rect Root = Leaf {parent = Root, mbr = rect, lhv = hilbertValue rect, hrects = [fromRect rect]}
+insert rect nonleaf@(NonLeaf {}) = insert rect (chooseLeaf rect nonleaf)
+insert rect leaf@(Leaf {mbr = oldMbr})
+  | isFull leaf = error "handleOverflow" -- handleOverflow $ parent leaf
+  | otherwise = Leaf { parent = parent leaf
+                     , mbr = newMbr
+                     , lhv = maximum $ map (\(HRect h _) -> h) newHRects
+                     , hrects = newHRects
+                     }
   where
     newHRects = insertBy (\(HRect h0 _) (HRect h1 _) -> compare h0 h1) (fromRect rect) (hrects leaf)
+    newMbr = Rect { rectMinX = min (rectMinX rect) (rectMinX oldMbr)
+                  , rectMinY = min (rectMinY rect) (rectMinY oldMbr)
+                  , rectMaxX = max (rectMaxX rect) (rectMaxX oldMbr)
+                  , rectMaxY = max (rectMaxY rect) (rectMaxY oldMbr)
+                  }
     
----- returns the Node
+--handleOverflow :: Node -> Rect -> (Node, Maybe Node)
+--handleOverflow parentNode rect = newParent
+
+
 chooseLeaf :: Rect -> Node -> Node
-chooseLeaf _ leaf@(Leaf _ _) = leaf
+chooseLeaf _ leaf@(Leaf {}) = leaf
 chooseLeaf rect nonleaf 
-  | length bestLeaves == 0 = trace "aha - length bestLeaves == 0" $ last (nodes nonleaf)
+  | length bestLeaves == 0 = trace "aha - length bestLeaves == 0" $ last (children nonleaf)
   | otherwise              = chooseLeaf rect $ head bestLeaves
   where
-    bestLeaves = dropWhile (\x -> lhv x <= hilbertValue rect) (nodes nonleaf)
+    bestLeaves = dropWhile (\x -> lhv x <= hilbertValue rect) (children nonleaf)
