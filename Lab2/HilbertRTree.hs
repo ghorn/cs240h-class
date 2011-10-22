39ba16a110ac11bd7d3c4039cece01467e270320
Greg Horn <gregmainland@gmail.com>
2011-10-22 02:09:11 -0700

    implemented ZNode, IsZNode

--- Lab2/HilbertRTree.hs
+++ Lab2/HilbertRTree.hs
@@ -73,8 +73,22 @@ data Leaf = Leaf (Node HRect) deriving Show
 data Nonleaf = Nonleaf (Node Child) deriving Show
 data Root = Root [Nonleaf] deriving Show
 
-data ZNonleaf = ZNonleaf (Maybe ZNonleaf) ([Nonleaf], [Nonleaf]) Nonleaf
-data ZLeaf = ZLeaf ZNonleaf ([Leaf], [Leaf]) Leaf
+--data ZNonleaf = ZNonleaf (Maybe ZNonleaf) ([Nonleaf], [Nonleaf]) Nonleaf
+--data ZLeaf = ZLeaf ZNonleaf ([Leaf], [Leaf]) Leaf
+
+data ZNode a b = ZNode b ([a], [a]) a
+data ZLeaf = ZLeaf (ZNode Leaf ZNonleaf)
+data ZNonleaf = ZNonleaf (ZNode Nonleaf (Maybe ZNonleaf))
+
+class IsZNode a b c | a -> b c where
+  getParent :: a -> c
+  getSiblings :: a -> ([b], [b])
+  getFocus :: a -> b
+  
+instance IsZNode ZLeaf Leaf ZNonleaf where
+  getParent (ZLeaf (ZNode parent _ _)) = parent
+  getSiblings (ZLeaf (ZNode _ sibs _)) = sibs
+  getFocus (ZLeaf (ZNode _ _ focus)) = focus
 
 
 fromRect :: Rect -> HRect
@@ -106,70 +120,62 @@ newHRTree :: [Rect] -> Root
 newHRTree [] = error "sorry, you have to insert something in me with non-zero length"
 newHRTree (rect0:rects) = foldr insert firstRoot rects
   where
-    firstRoot = zipup $ ZNonleaf Nothing ([], []) firstNonleaf
+    firstRoot = zipup $ ZNonleaf (ZNode Nothing ([], []) firstNonleaf)
     firstNonleaf = makeNonleaf [CLeaf firstLeaf]
       where
         firstLeaf = makeLeaf [fromRect rect0]
 
 zipup :: ZNonleaf -> Root
-zipup (ZNonleaf Nothing (lsibs, rsibs) focus) = Root $ lsibs ++ (focus:rsibs)
+zipup (ZNonleaf (ZNode Nothing (lsibs, rsibs) focus)) = Root $ lsibs ++ (focus:rsibs)
 zipup z = zipup $ zipupOnce z
 
 -- take focus NonleafNode, return parent's ZNode updating new focus's mbr and lhv
 zipupOnce :: ZNonleaf -> ZNonleaf
-zipupOnce (ZNonleaf Nothing _ _) = error "this should probably return self"
-zipupOnce (ZNonleaf (Just oldParentNode) (lsibs, rsibs) focus) = newZNode
+zipupOnce (ZNonleaf (ZNode Nothing _ _)) = error "this should probably return self"
+zipupOnce (ZNonleaf (ZNode (Just oldParentNode) (lsibs, rsibs) focus)) = newZNode
   where
-    ZNonleaf grandparent (parentLsibs, parentRsibs) _ = oldParentNode
-    newZNode = ZNonleaf grandparent (parentLsibs, parentRsibs) newNonleaf
+    ZNonleaf (ZNode grandparent (parentLsibs, parentRsibs) _) = oldParentNode
+    newZNode = ZNonleaf (ZNode grandparent (parentLsibs, parentRsibs) newNonleaf)
       where
         newNonleaf = makeNonleaf $ map CNonleaf allSiblings
           where
             allSiblings = lsibs ++ (focus:rsibs)
 
 zipupLeaf :: ZLeaf -> ZNonleaf
-zipupLeaf (ZLeaf oldParentNode (lsibs, rsibs) focus) = newZNode
+zipupLeaf (ZLeaf (ZNode oldParentNode (lsibs, rsibs) focus)) = newZNode
   where
-    ZNonleaf grandparent (parentLsibs, parentRsibs) _ = oldParentNode
-    newZNode = ZNonleaf grandparent (parentLsibs, parentRsibs) newNonleaf
+    ZNonleaf (ZNode grandparent (parentLsibs, parentRsibs) _) = oldParentNode
+    newZNode = ZNonleaf (ZNode grandparent (parentLsibs, parentRsibs) newNonleaf)
       where
         newNonleaf = makeNonleaf $ map CLeaf allSiblings
           where
             allSiblings = lsibs ++ (focus:rsibs)
 
 
---handleNonleafOverflow :: HasChildren a Child => ZNonleaf -> a -> Root
---handleNonleafOverflow :: ZNonleaf -> Nonleaf -> Root
-handleNonleafOverflow (ZNonleaf parent (lsibs, rsibs) self) nn
-  -- if there was space to redistribute, then do it
-  | newNumberOfNonleaves <= cN = zipup (ZNonleaf parent ([], bl1:bls) bl0)
-  -- if there is no space left, make new node and evenly redistribute all siblings
-  -- take the node with new smallest hilbert value and make it the child of a new node
-  -- call adjustTree with this new node
-  | otherwise = adjustTree (zipupOnce (ZNonleaf parent ([], bls) bl1)) $ makeNonleaf [CNonleaf bl0]
+--zipupNodeOnce :: ZLeaf -> ZNonleaf
+--zipupNodeOnce (ZLeaf (ZNode oldParentNode (lsibs, rsibs) focus)) = newZNode
+zipupNodeOnce znode = newZNode
   where
-    newNumberOfNonleaves
-      -- if any nonleaf has space, don't add a node
-      | any (not . isFull) allSiblings = length allSiblings
-      | otherwise                      = length allSiblings + 1
-
-    allSiblings = lsibs ++ (self:rsibs)
-    
-    (bl0:bl1:bls) = map makeNonleaf (evenlyDistribute newNumberOfNonleaves newChildren)
-    newChildren = foldr insertByLhv oldChildren (children nn)
-    oldChildren = concat $ map children allSiblings
+    (ZNode grandparent (parentLsibs, parentRsibs) _) = getParent znode
+    newZNode = ZNonleaf (ZNode grandparent (parentLsibs, parentRsibs) newNonleaf)
+      where
+        newNonleaf = makeNonleaf $ map CLeaf allSiblings
+          where
+            allSiblings = lsibs ++ (focus:rsibs)
+            (lsibs, rsibs) = getSiblings znode
+            focus = getFocus znode
 
 
 adjustTree :: ZNonleaf -> Nonleaf -> Root
-adjustTree n@(ZNonleaf parent (lsibs, rsibs) self) nn
+adjustTree n@(ZNonleaf (ZNode parent (lsibs, rsibs) self)) nn
   -- if there was an extra space, simply insert the node
-  | newNumberOfNonleaves <  cN = zipup (ZNonleaf parent ([], bl1:bls) bl0)
+  | newNumberOfNonleaves <  cN = zipup (ZNonleaf (ZNode parent ([], bl1:bls) bl0))
   -- otherwise call handleNonleafOverflow
   | newNumberOfNonleaves == cN = handleNonleafOverflow n nn
   -- if a split caused an overflow, evenly redistribute all siblings
   -- take the nonleaf with new smallest hilbert value and make it the child of a new nonnonleaf node
   -- call adjustTree with this new nonnonleaf node
-  | otherwise = adjustTree (zipupOnce (ZNonleaf parent ([], bls) bl1)) $ makeNonleaf [CNonleaf bl0]
+  | otherwise = adjustTree (zipupOnce (ZNonleaf (ZNode parent ([], bls) bl1))) $ makeNonleaf [CNonleaf bl0]
   where
     allSiblings = lsibs ++ (self:rsibs)
     newNumberOfNonleaves
@@ -185,14 +191,36 @@ adjustTree n@(ZNonleaf parent (lsibs, rsibs) self) nn
     newChildren = foldr insertByLhv oldChildren (children nn)
     oldChildren = concat $ map children allSiblings
     
+--handleNonleafOverflow :: HasChildren a Child => ZNonleaf -> a -> Root
+--handleNonleafOverflow :: ZNonleaf -> Nonleaf -> Root
+handleNonleafOverflow (ZNonleaf (ZNode parent (lsibs, rsibs) self)) nn
+  -- if there was space to redistribute, then do it
+  | newNumberOfNonleaves <= cN = zipup (ZNonleaf (ZNode parent ([], bl1:bls) bl0))
+  -- if there is no space left, make new node and evenly redistribute all siblings
+  -- take the node with new smallest hilbert value and make it the child of a new node
+  -- call adjustTree with this new node
+  | otherwise = adjustTree (zipupOnce (ZNonleaf (ZNode parent ([], bls) bl1))) $ makeNonleaf [CNonleaf bl0]
+  where
+    newNumberOfNonleaves
+      -- if any nonleaf has space, don't add a node
+      | any (not . isFull) allSiblings = length allSiblings
+      | otherwise                      = length allSiblings + 1
+
+    allSiblings = lsibs ++ (self:rsibs)
+    
+    (bl0:bl1:bls) = map makeNonleaf (evenlyDistribute newNumberOfNonleaves newChildren)
+    newChildren = foldr insertByLhv oldChildren (children nn)
+    oldChildren = concat $ map children allSiblings
+
+
 handleLeafOverflow :: ZLeaf -> HRect -> Root
-handleLeafOverflow (ZLeaf parent (lsibs, rsibs) self) hrect
-  -- if no split caused a overflow, evenly redistribute all children by hilbert value
-  | newNumberOfLeaves <= cN = zipup $ zipupLeaf (ZLeaf parent ([], bl1:bls) bl0)
-  -- if a node split caused an overflow evenly redistribute all children by hilbet value
-  -- take the leaf with new smallest hilbert value and make it the child of a new nonleaf node
+handleLeafOverflow (ZLeaf (ZNode parent (lsibs, rsibs) self)) hrect
+  -- if there is space to redistribute, then do it
+  | newNumberOfLeaves <= cN = zipup $ zipupLeaf (ZLeaf (ZNode parent ([], bl1:bls) bl0))
+  -- if there is no space left, make a new node and redistribute all siblings
+  -- take the node with new smallest hilbert value and make it the child of a new node
   -- call adjustTree with this new nonleaf node
-  | otherwise = adjustTree (zipupLeaf (ZLeaf parent ([], bls) bl1)) $ makeNonleaf [CLeaf bl0]
+  | otherwise = adjustTree (zipupLeaf (ZLeaf (ZNode parent ([], bls) bl1))) $ makeNonleaf [CLeaf bl0]
   where
     -- if any leaf has space, redistribute evenly
     newNumberOfLeaves
@@ -204,39 +232,40 @@ handleLeafOverflow (ZLeaf parent (lsibs, rsibs) self) hrect
     newHRects = insertByLhv hrect $ concat $ map children allSiblings
     
 
+
 insert :: Rect -> Root -> Root
 insert rect root = insertInLeaf hrect (chooseLeafFromRoot hrect root)
   where
     hrect = fromRect rect
 
 insertInLeaf :: HRect -> ZLeaf -> Root
-insertInLeaf hrect zleaf@(ZLeaf parent (leftSiblings, rightSiblings) oldleaf)
+insertInLeaf hrect zleaf@(ZLeaf (ZNode parent (leftSiblings, rightSiblings) oldleaf))
   | isFull oldleaf = handleLeafOverflow zleaf hrect
-  | otherwise      = zipup $ zipupLeaf $ ZLeaf parent (leftSiblings, rightSiblings) newleaf
+  | otherwise      = zipup $ zipupLeaf $ ZLeaf (ZNode parent (leftSiblings, rightSiblings) newleaf)
   where
     newleaf = makeLeaf newHRects
     newHRects = insertByLhv hrect (children oldleaf)
 
 -- choose the best sibling from the root, construct the zipper, and call zChooseLeaf
 chooseLeafFromRoot :: HasLhv a => a -> Root -> ZLeaf
-chooseLeafFromRoot hrect (Root topSiblings) = chooseLeafFromZNonleaf hrect $ ZNonleaf Nothing (lsibs, rsibs) focus
+chooseLeafFromRoot hrect (Root topSiblings) = chooseLeafFromZNonleaf hrect $ ZNonleaf (ZNode Nothing (lsibs, rsibs) focus)
   where
     (lsibs, focus, rsibs) = breaker hrect topSiblings
     
 chooseLeafFromZNonleaf :: HasLhv a => a -> ZNonleaf -> ZLeaf
 -- return on finding a leaf node
-chooseLeafFromZNonleaf _ (ZNonleaf _ _ (Nonleaf Node {nodeChildren = []})) = error "empty children yo"
-chooseLeafFromZNonleaf hrect searchme@(ZNonleaf _ _ (Nonleaf Node {nodeChildren = ch@((CLeaf _):_)})) = out
+chooseLeafFromZNonleaf _ (ZNonleaf (ZNode _ _ (Nonleaf Node {nodeChildren = []}))) = error "empty children yo"
+chooseLeafFromZNonleaf hrect searchme@(ZNonleaf (ZNode _ _ (Nonleaf Node {nodeChildren = ch@((CLeaf _):_)}))) = out
   where
-    out = ZLeaf searchme (lsibs, rsibs) focus
+    out = ZLeaf (ZNode searchme (lsibs, rsibs) focus)
     (lsibs, focus, rsibs) = breaker hrect $ map getChild ch
       where
         getChild (CLeaf x) = x
         getChild (CNonleaf _) = error "shouldn't ever be a nonleaf here"
 -- normal search
-chooseLeafFromZNonleaf hrect searchme@(ZNonleaf _ _ (Nonleaf Node {nodeChildren = ch@((CNonleaf _):_)})) = out
+chooseLeafFromZNonleaf hrect searchme@(ZNonleaf (ZNode _ _ (Nonleaf Node {nodeChildren = ch@((CNonleaf _):_)}))) = out
   where
-    out = chooseLeafFromZNonleaf hrect $ ZNonleaf (Just searchme) (lsibs, rsibs) focus
+    out = chooseLeafFromZNonleaf hrect $ ZNonleaf (ZNode (Just searchme) (lsibs, rsibs) focus)
     (lsibs, focus, rsibs) = breaker hrect $ map getChild ch
       where
         getChild (CNonleaf x) = x
